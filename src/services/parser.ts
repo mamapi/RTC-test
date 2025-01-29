a6856aa21df9cbe5df4f736409609690d4bf1931
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-01-29 12:27:02 +0100

    test: add failing test to parse odds

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -1,7 +1,22 @@
 export type MappingDictionary = Record<string, string>;
 
+export type SportEventOdd = {
+  id: string;
+  sportId: string;
+  competitionId: string;
+  startTime: string;
+  homeCompetitorId: string;
+  awayCompetitorId: string;
+  status: string;
+  scores: string;
+};
+
 export const parseMappings = (rawMappings: string): MappingDictionary => {
   return Object.fromEntries(
     rawMappings.split(";").map((entry) => entry.split(":"))
   );
+};
+
+export const parseOdds = (rawOdds: string): SportEventOdd[] => {
+  return [];
 };
--- test/services/parser.test.ts
+++ test/services/parser.test.ts
@@ -1,5 +1,10 @@
 import { describe, expect, it } from "vitest";
-import { MappingDictionary, parseMappings } from "../../src/services/parser";
+import {
+  MappingDictionary,
+  SportEventOdd,
+  parseMappings,
+  parseOdds,
+} from "../../src/services/parser";
 
 describe("Parser", () => {
   it("should parse the mappings correctly", () => {
@@ -13,5 +18,40 @@ describe("Parser", () => {
     };
     const parsedMappings = parseMappings(rawMappings);
     expect(parsedMappings).toEqual(expectedMappings);
+  });
+
+  it("should parse the odds correctly", () => {
+    const odd1 =
+      "sportEventId1,sportId1,cometitionId1,1709900432183,homeTeamId1,awayTeamId1,statusId1,periodId1@1:1|periodId2@1:2";
+
+    const odd2 =
+      "sportEventId2,sportId2,cometitionId2,1709900432180,homeTeamId2,awayTeamId2,statusId2,periodId2@0:0|periodId2@0:2";
+
+    const odds = `${odd1}\n${odd2}`;
+
+    const expectedOdds: SportEventOdd[] = [
+      {
+        id: "sportEventId1",
+        sportId: "sportId1",
+        competitionId: "cometitionId1",
+        startTime: "1709900432183",
+        homeCompetitorId: "homeTeamId1",
+        awayCompetitorId: "awayTeamId1",
+        status: "statusId1",
+        scores: "periodId1@1:1|periodId2@1:2",
+      },
+      {
+        id: "sportEventId2",
+        sportId: "sportId2",
+        competitionId: "cometitionId2",
+        startTime: "1709900432180",
+        homeCompetitorId: "homeTeamId2",
+        awayCompetitorId: "awayTeamId2",
+        status: "statusId2",
+        scores: "periodId2@0:0|periodId2@0:2",
+      },
+    ];
+    const parsedOdds = parseOdds(odds);
+    expect(parsedOdds).toEqual(expectedOdds);
   });
 });
