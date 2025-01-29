cddba32cb3a4840d251c3757f1eae2c103734704
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-01-29 20:02:03 +0100

    feat: initial Hapi server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello world");
+import Hapi from "@hapi/hapi";
+
+export let server: Hapi.Server;
+
+export const init = async (): Promise<Hapi.Server> => {
+  server = Hapi.server({
+    port: Number(process.env.PORT) || 4000,
+    host: "localhost",
+  });
+
+  return server;
+};
+
+export const start = async () => {
+  console.log(`Listening on ${server.settings.host}:${server.settings.port}`);
+  await server.start();
+};
+
+init()
+  .then(start)
+  .catch((err) => console.error("Error while starting server", err));
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,34 @@
-import { beforeEach, describe, expect, it } from 'vitest';
-describe('index', () => {
-  it('should run test', () => {
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { server, init } from "../src";
+
+describe("index", () => {
+  it("should run test", () => {
     expect(true).toBe(true);
+  });
+});
+
+describe("Server tests", () => {
+  beforeEach(async () => {
+    await init();
+  });
+
+  afterEach(async () => {
+    await server.stop();
+  });
+
+  it("server initializes correctly", async () => {
+    expect(server).toBeDefined();
+    expect(server.settings.port).toBe(4000);
+    expect(server.settings.host).toBe("localhost");
+  });
+
+  it("server handles environment port variable", async () => {
+    process.env.PORT = "4001";
+
+    await server.stop();
+    const newServer = await init();
+
+    expect(newServer.settings.port).toBe(4001);
+    delete process.env.PORT;
   });
 });
