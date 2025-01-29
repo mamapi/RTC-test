1f196aa041cdbe224ae53461ac5394f8f20fc560
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-01-29 17:51:13 +0100

    feat: remove events from state when not present in updates

--- src/services/stateManager.ts
+++ src/services/stateManager.ts
@@ -8,8 +8,15 @@ export const updateState = (odds: SportEventOdd[], mappings: MappingDictionary) 
   for (const [key, value] of Object.entries(sportEvents)) {
     internalState[key] = value;
   }
+
+  for (const key in internalState) {
+    if (!Object.keys(sportEvents).some((id) => id === key)) {
+      internalState[key].status = "REMOVED";
+    }
+  }
 };
 
 export const geAllEvents = () => internalState;
 
-export const getActiveEvents = () => internalState;
+export const getActiveEvents = () =>
+  Object.fromEntries(Object.entries(internalState).filter(([_, event]) => event.status !== "REMOVED"));
