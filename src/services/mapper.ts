d8fc67bc3e564ae5d3fa95c5c3e1c5f9e293f541
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-01-29 15:16:28 +0100

    refactor: reorganize sport events mapping logic

--- src/services/mapper.ts
+++ src/services/mapper.ts
@@ -28,37 +28,41 @@ export type SportEvent = {
 export const mapSportEvents = (odds: SportEventOdd[], mappings: MappingDictionary): SportEvents => {
   const sportEvents: SportEvents = {};
   for (const odd of odds) {
-    const scores = odd.scores.reduce((acc, score) => {
-      acc[score.periodId] = {
-        type: score.periodId,
-        home: score.homeScore,
-        away: score.awayScore,
-      };
-      return acc;
-    }, {} as Record<PeriodId, { type: PeriodId; home: string; away: string }>);
-
-    const competitors = {
-      HOME: {
-        type: "HOME" as const,
-        name: mappings[odd.homeCompetitorId],
-      },
-      AWAY: {
-        type: "AWAY" as const,
-        name: mappings[odd.awayCompetitorId],
-      },
-    };
-
-    const sportEvent: SportEvent = {
-      id: odd.id,
-      status: mappings[odd.status] as SportEventStatus,
-      scores,
-      startTime: odd.startTime,
-      sport: mappings[odd.sportId],
-      competition: mappings[odd.competitionId],
-      competitors,
-    };
-
-    sportEvents[odd.id] = sportEvent;
+    sportEvents[odd.id] = mapSportEvent(odd, mappings);
   }
   return sportEvents;
+};
+
+const mapSportEvent = (odd: SportEventOdd, mappings: MappingDictionary): SportEvent => {
+  const scores = odd.scores.reduce((acc, score) => {
+    acc[score.periodId] = {
+      type: score.periodId,
+      home: score.homeScore,
+      away: score.awayScore,
+    };
+    return acc;
+  }, {} as Record<PeriodId, { type: PeriodId; home: string; away: string }>);
+
+  const competitors = {
+    HOME: {
+      type: "HOME" as const,
+      name: mappings[odd.homeCompetitorId],
+    },
+    AWAY: {
+      type: "AWAY" as const,
+      name: mappings[odd.awayCompetitorId],
+    },
+  };
+
+  const sportEvent: SportEvent = {
+    id: odd.id,
+    status: mappings[odd.status] as SportEventStatus,
+    scores,
+    startTime: odd.startTime,
+    sport: mappings[odd.sportId],
+    competition: mappings[odd.competitionId],
+    competitors,
+  };
+
+  return sportEvent;
 };
