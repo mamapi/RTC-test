1f9ad1c76a1eda2f8a5d13898cb481b93bda1747
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-01-30 15:18:39 +0100

    refactor: remove unnecessary try-catch from formatDate

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -2,10 +2,6 @@ export const formatDate = (timestamp: number | string | null) => {
   if (isNaN(Number(timestamp))) return null;
   if (!timestamp) return null;
 
-  try {
-    const date = new Date(Number(timestamp));
-    return date.toISOString();
-  } catch (error) {
-    return null;
-  }
+  const date = new Date(Number(timestamp));
+  return date.toISOString();
 };
