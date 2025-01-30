40e117a1abf0e0eadb27662d08df99a226d185af
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-01-30 13:01:34 +0100

    test: fix dateFormat test

--- test/utils/dateUtils.test.ts
+++ test/utils/dateUtils.test.ts
@@ -3,9 +3,12 @@ import { formatDate } from "../../src/utils/dateUtils";
 
 describe("dateUtils", () => {
   it.each([
-    { input: 1738191600, expected: "2025-01-30T00:00:00.000Z" },
-    { input: 1735690200, expected: "2025-01-01T01:10:00.000Z" },
+    { input: 1738195200000, expected: "2025-01-30T00:00:00.000Z" },
+    { input: "1738195200000", expected: "2025-01-30T00:00:00.000Z" },
+    { input: 1735690200000, expected: "2025-01-01T00:10:00.000Z" },
+    { input: "1735690200000", expected: "2025-01-01T00:10:00.000Z" },
     { input: 0, expected: "1970-01-01T00:00:00.000Z" },
+    { input: "0", expected: "1970-01-01T00:00:00.000Z" },
   ])("should format timestamp $input to ISO string", ({ input, expected }) => {
     expect(formatDate(input)).toEqual(expected);
   });
