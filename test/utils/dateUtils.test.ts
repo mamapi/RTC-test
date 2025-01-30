59b84d7d8a435d7fcea37a1f5303cd963807d500
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-01-30 12:48:25 +0100

    test: add failing test to format date

---
+++ test/utils/dateUtils.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+import { formatDate } from "../../src/utils/dateUtils";
+
+describe("dateUtils", () => {
+  it.each([
+    { input: 1738191600, expected: "2025-01-30T00:00:00.000Z" },
+    { input: 1735690200, expected: "2025-01-01T01:10:00.000Z" },
+    { input: 0, expected: "1970-01-01T00:00:00.000Z" },
+  ])("should format timestamp $input to ISO string", ({ input, expected }) => {
+    expect(formatDate(input)).toEqual(expected);
+  });
+});
