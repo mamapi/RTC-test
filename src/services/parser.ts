9b746be1476b1a7dab69b220ad507d2718155fe1
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-01-31 23:18:27 +0100

    refactor: extract parseEvent method

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -1,32 +1,38 @@
 import { MappingDict, SportEvent, PeriodScore } from "../models/rowModel";
 
 export const parseMappings = (rawMappings?: string | null): MappingDict => {
-  if (!rawMappings?.trim()) throw new Error("Cannot parse mappings: Invalid mappings input");
+  if (!rawMappings?.trim()) {
+    throw new Error("Cannot parse mappings: Invalid mappings input");
+  }
 
   return Object.fromEntries(rawMappings.split(";").map((entry) => entry.split(":")));
 };
 
 export const parseEvents = (rawEvents?: string | null): SportEvent[] => {
-  if (!rawEvents?.trim()) throw new Error("Cannot parse events: Invalid events input");
+  if (!rawEvents?.trim()) {
+    throw new Error("Cannot parse events: Invalid events input");
+  }
 
-  const events = rawEvents.split("\n").map((event, index) => {
-    const fields = event.split(",");
+  return rawEvents.split("\n").map((rawEvent, index) => parseEvent(rawEvent, index));
+};
 
-    if (fields.length < 7) throw new Error(`Cannot parse events: Insufficient number of fields at line ${index + 1}`);
+const parseEvent = (rawEvent: string, index: number): SportEvent => {
+  const fields = rawEvent.split(",");
 
-    const [id, sportId, competitionId, startTime, homeCompetitorId, awayCompetitorId, status, scores] = fields;
-    return {
-      id,
-      sportId,
-      competitionId,
-      startTime,
-      homeCompetitorId,
-      awayCompetitorId,
-      status,
-      scores: parseScores(scores),
-    };
-  });
-  return events;
+  if (fields.length < 7) throw new Error(`Cannot parse events: Insufficient number of fields at line ${index + 1}`);
+
+  const [id, sportId, competitionId, startTime, homeCompetitorId, awayCompetitorId, status, scores] = fields;
+
+  return {
+    id,
+    sportId,
+    competitionId,
+    startTime,
+    homeCompetitorId,
+    awayCompetitorId,
+    status,
+    scores: parseScores(scores),
+  };
 };
 
 const parseScores = (scores?: string): PeriodScore[] => {
