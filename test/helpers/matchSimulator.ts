ff8cf5f35c3af977fe2fc144203c8886e74b3f28
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-02-01 15:11:37 +0100

    feat: handle periods tracking

--- test/helpers/matchSimulator.ts
+++ test/helpers/matchSimulator.ts
@@ -4,14 +4,20 @@ type Team = "home" | "away";
 class Match {
   private status: MatchStatus;
   private currentScore: { home: number; away: number };
+  private scores: Record<number, { home: number; away: number }>;
+  private currentPeriod: number;
 
   constructor() {
     this.status = "PRE";
+    this.scores = {};
   }
 
   start() {
     this.status = "IN";
     this.currentScore = { home: 0, away: 0 };
+
+    this.currentPeriod = 1;
+    this.scores[this.currentPeriod] = { home: 0, away: 0 };
   }
 
   getStatus() {
@@ -20,10 +26,20 @@ class Match {
 
   score(team: Team, points: number = 1) {
     this.currentScore[team] += points;
+    this.scores[this.currentPeriod][team] += points;
   }
 
   getCurrentScore() {
     return this.currentScore;
+  }
+
+  getScores() {
+    return this.scores;
+  }
+
+  startNewPeriod() {
+    this.currentPeriod++;
+    this.scores[this.currentPeriod] = { home: 0, away: 0 };
   }
 }
 
