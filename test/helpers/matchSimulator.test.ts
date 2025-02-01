dccff9cc2db4cea187583cbb58955fb3ef1336d8
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-02-01 14:22:29 +0100

    test: add initial tests for match simulator

---
+++ test/helpers/matchSimulator.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from "vitest";
+
+describe("Match Simulator", () => {
+  it("should simulate pre match", () => {
+    const match = new Match();
+    expect(match.getStatus()).toBe("PRE");
+  });
+
+  it("should simulate start match", () => {
+    const match = new Match();
+    match.start();
+    expect(match.getStatus()).toBe("IN");
+  });
+});
