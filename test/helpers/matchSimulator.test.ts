9c9a55c782df168510813a450fd7f504dd44a36a
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-02-01 15:11:19 +0100

    test: handle cases for periods tracking

--- test/helpers/matchSimulator.test.ts
+++ test/helpers/matchSimulator.test.ts
@@ -16,15 +16,32 @@ describe("Match Simulator", () => {
   it("should simulate score", () => {
     const match = new Match();
     match.start();
-    
+
     match.score("home");
     expect(match.getStatus()).toBe("IN");
     expect(match.getCurrentScore()).toStrictEqual({ home: 1, away: 0 });
+    expect(match.getScores()).toStrictEqual({ 1: { home: 1, away: 0 } });
 
     match.score("home", 2);
     expect(match.getCurrentScore()).toStrictEqual({ home: 3, away: 0 });
+    expect(match.getScores()).toStrictEqual({ 1: { home: 3, away: 0 } });
 
     match.score("away", 2);
     expect(match.getCurrentScore()).toStrictEqual({ home: 3, away: 2 });
+    expect(match.getScores()).toStrictEqual({ 1: { home: 3, away: 2 } });
+
+    match.startNewPeriod();
+    expect(match.getCurrentScore()).toStrictEqual({ home: 3, away: 2 });
+    expect(match.getScores()).toStrictEqual({
+      1: { home: 3, away: 2 },
+      2: { home: 0, away: 0 },
+    });
+
+    match.score("home");
+    expect(match.getCurrentScore()).toStrictEqual({ home: 4, away: 2 });
+    expect(match.getScores()).toStrictEqual({
+      1: { home: 3, away: 2 },
+      2: { home: 1, away: 0 },
+    });
   });
 });
