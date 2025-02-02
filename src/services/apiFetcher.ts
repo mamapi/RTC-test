f2d7b085a079059afa2b3a8c18eabd0d8b2135e1
Grzegorz Gorczynski <gorczynski.grzegorz@gmail.com>
2025-02-02 17:30:18 +0100

    feat: prevent overlapping ticks when execution time is too long

--- src/services/apiFetcher.ts
+++ src/services/apiFetcher.ts
@@ -5,11 +5,24 @@ import { updateState } from "./stateManager";
 
 class ApiFetcher {
   private intervalId: NodeJS.Timeout | null = null;
+  private isRunning = false;
 
   constructor(private readonly apiClient: ApiClient, private readonly intervalMs: number) {}
 
   start() {
-    this.intervalId = setInterval(() => this.onTick(), this.intervalMs);
+    this.intervalId = setInterval(async () => {
+      if (this.isRunning) {
+        Logger.warn("API fetcher is already running");
+        return;
+      }
+
+      this.isRunning = true;
+      try {
+        await this.onTick();
+      } finally {
+        this.isRunning = false;
+      }
+    }, this.intervalMs);
   }
 
   private async onTick() {
